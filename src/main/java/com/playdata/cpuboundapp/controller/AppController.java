3532573bfeb46fdd39572fd73b7c92b7afcb212c
babogyeong <122768710+babogyeong@users.noreply.github.com>
2024-07-02 11:15:54 +0900

    Update AppController.java

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "webhook test restart detach mode rolling deploy, !sleep 60, 3, 4 instance only";
+        return "webhoot restadmekfwlfhfho;fuaehufe;ffwp9efay, !sleep 60, 3, 4 instance only";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
